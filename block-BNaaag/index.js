0e4288feaad92dee2bc5719d7a751c052bbccd3f
Akash Singh <akashingh95@gmail.com>
2022-03-14 22:37:18 +0530

    update|buffer|node

--- block-BNaaag/index.js
+++ block-BNaaag/index.js
@@ -1,23 +1,23 @@
-var fs = require('fs');
+// var fs= require('fs');
 
-fs.readFile('./content.md',(err ,content) => {
+// fs.readFile('./content.md',(err ,content) => {
 
-  console.log(content.toString());
-})
-
-
-// sync
-console.log('first');
-console.log('second');
-
-//Async
+//   console.log(content.toString());
+// })
 
 
-setTimeout(() => {
-    console.log('second');  
-},2000)
+// // sync
+// console.log('first');
+// console.log('second');
 
-console.log('first');
+// //Async
+
+
+// setTimeout(() => {
+//     console.log('second');  
+// },2000)
+
+// console.log('first');
 
 
 //Buffer
@@ -27,3 +27,14 @@ console.log(buff1);
  buff1.write('Welcome to Buffer');
 console.log(buff1.toString())
 
+var {readFile ,readFileSync} = require('fs');
+readFile('./content.md', 'utf8',(err ,content) => {
+
+       console.log(content);
+     })
+
+     var result = readFileSync('./content.md', 'utf8',(err ,content) => {
+
+        console.log(content);
+      })
+console.log(result)
